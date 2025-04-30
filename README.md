d8e9c0a711828ab7cbcfd275689d0d32c6f9aa18
Maria-Costa <dudanilde@icloud.com>
2025-04-30 02:54:18 -0300

    Atualização do README com as mudanças feitas e melhorias no código

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 Este é um projeto de calculadora realizado em pyhton. Nesta versão simples, ela realiza apenas as quatro operações básicas de **soma, subtração, multiplicação e divisão**.
 
+# Atualizações:
+- Permição para realizar mais operações sem reiniciar o programa
+- Validação de entrada numérica com `try/except`
+- Verificação de operação válida antes de continuar
+
 ## Para utilizar:
 
 1. Garanta que o Python está instalado na sua máquina
 2. Execute o código no terminal:
 ```bash
 python calculadora.py
+
--- calculadora.py
+++ calculadora.py
@@ -16,25 +16,44 @@ def dividir (x, y):
 
 print ('Seja bem-vindo à calculadora!')
 
-opr = input("Escolha a operação (+, -, *, /): ")
-n1 = float(input("Digite o primeiro número da operação: "))
-n2 = float(input("Digite o segundo número da operação: "))
+#permitir que o usuário realize mais de uma operação
+operacoes_validas = ['+', '-', '*', '/']
+while True:
+    print("Operações disponíveis (+, -, *, /): ")
+    while True:
+        opr = input("\nEscolha a operação (+, -, *, /): ")
+        if opr in operacoes_validas:
+            break
+        else:
+            print("Operação inválida! Tente novamente.")
+
+    try:
+        n1 = float(input("Digite o primeiro número da operação: "))
+        n2 = float(input("Digite o segundo número da operação: "))
+    except ValueError:
+        print("Entrada não permitida! Por favor, digite números válidos")
+        continue  # volta pro início do loop
 
 #chamando as funções e variáveis
 
-if opr == '+':
-    resp = somar(n1, n2)
+    if opr == '+':
+        resp = somar(n1, n2)
 
-elif opr == '-':
-    resp = subtrair(n1, n2)
+    elif opr == '-':
+        resp = subtrair(n1, n2)
 
-elif opr == '*':
-    resp = multiplicar(n1, n2)
+    elif opr == '*':
+        resp = multiplicar(n1, n2)
 
-elif opr == '/':
-    resp = dividir(n1, n2)
+    elif opr == '/':
+        resp = dividir(n1, n2)
 
-else: 
-    resp = "Operação inválida! Tente novamente"
+    else: 
+        resp = "Operação inválida! Tente novamente"
 
-print("A resposta é:", resp)
+    print("A resposta é:", resp)
+
+    repetir = input("\nDeseja realizar outra operação? (s/n): ")
+    if repetir.lower() != 's':
+        print("Até a próxima!")
+        break
